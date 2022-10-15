b4b6b7e0f679ab7c35cae553ab918b04146d87d9
xLyMzR <leomardomamedeee@protonmail.com>
2022-10-15 15:31:41 -0300

    Criação Atributos e Construtor - Cliente.java

--- codigo/FlyCore/src/Clientes/Cliente.java
+++ codigo/FlyCore/src/Clientes/Cliente.java
@@ -1,6 +1,17 @@
 package Clientes;
+import java.util.LinkedList;
+import codigo.FlyCore.src.*;
 public class Cliente {
 
+    private final String nome = " ";
+    private final int NUM_DOCUMENTO=0;
+    private int pontuacaoCliente;
+    private LinkedList<Bilhete> bilhetesCliente;
 
+    public Cliente(String nome_cliente, int num_doc){
+        this.nome = nome;
+        this.NUM_DOCUMENTO = num_doc;
+        this.pontuacaoCliente=0;
+    }
 
 }
