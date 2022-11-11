c69ff070a880f2351fcb30b50d0df2f28ef3e544
xLyMzR <leomardomamedeee@protonmail.com>
2022-11-11 15:14:32 -0300

    OK

--- codigo/FlyCore/src/Utilitarios/AceleradorPts/Emulti.java
+++ codigo/FlyCore/src/Utilitarios/AceleradorPts/Emulti.java
@@ -1,16 +1,5 @@
 package Utilitarios.AceleradorPts;
 
 public enum Emulti {
-    PRATA(0), PRETO(1);
-
-    private final int valor;
-
-    Emulti(int valorOpcao){
-        valor = valorOpcao;
-    }
-
-    public int getValor(){
-        return valor;
-    }
-
+    PRATA, PRETO;
 }
