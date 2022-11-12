c088cdab8b06731078cb1133affe8f98f00fb326
xLyMzR <leomardomamedeee@protonmail.com>
2022-11-12 09:55:21 -0300

    Testes multiplicador - cliente

--- codigo/FlyCore/DiretorioTeste/ClienteTest/ClienteTest.java
+++ codigo/FlyCore/DiretorioTeste/ClienteTest/ClienteTest.java
@@ -10,6 +10,7 @@ import Passagens.Bilhete;
 import Passagens.Trecho;
 import Passagens.Voo;
 import Utilitarios.Data;
+import Utilitarios.AceleradorPts.Emulti;
 
 public class ClienteTest {
 
@@ -25,6 +26,20 @@ public class ClienteTest {
         blh1.inserirVoo(voo_1);
         cl.comprarBilhete(blh1);
         assertEquals(cl.getPontuacao(), 500);
+    }
+    @Test 
+    public void test_multiplicador_inicializado(){
+        Trecho tr1 = new Trecho("Belo Horizonte", "São Paulo");
+        Data data1 = new Data(06, 05, 2003);
+        Voo voo_1 = new Voo(tr1, data1, 500);
+        blh1.inserirVoo(voo_1);
+        cl.comprarBilhete(blh1);    
+        cl.setAcelerador(Emulti.PRETO);
+        //Teste com acelerador innicializado porém desativado!
+        assertEquals(cl.getPontuacao(), 500);
+        //Teste com acelerador ativado
+        cl.acelardor_pts.on_off();
+        assertEquals(cl.getPontuacao(), 750);
     }
     
 }
