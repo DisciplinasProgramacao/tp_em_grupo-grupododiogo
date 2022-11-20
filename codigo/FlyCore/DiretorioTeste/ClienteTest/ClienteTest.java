288b8920a7a29bf981de44d9a0cb688f4c2417b0
Lucas A <luckgreen161@gmail.com>
2022-11-20 14:59:03 -0300

    cliente     hotfix numDocumento -> cpf (string) 	feature calcularNumeroBilhetesPromocionais 	feature setNumeroBilhetesPromocionais 	feature comprarBilhete -> insere a data de compra do bilhete
    Bilhete
    	feature atributo dataDeCompra
    	feature inserirDataCompra
    	feature getDataCompra
    
    Data
    	hotfixc construtor dataAtual
    	feature tirar 1 ano da data
    
    Teste Cliente
    	add bilhetePontuacao
    	add bilhetePontuacaoZerada

--- codigo/FlyCore/DiretorioTeste/ClienteTest/ClienteTest.java
+++ codigo/FlyCore/DiretorioTeste/ClienteTest/ClienteTest.java
@@ -55,6 +55,6 @@ public class ClienteTest {
 
     @Test
     public void bilhetePontuacaoZerada(){
-        assertEquals(0, cl.calcularNumeroBilhetesPromocionais());
+        assertEquals(00, cl.calcularNumeroBilhetesPromocionais());
     }
 }
