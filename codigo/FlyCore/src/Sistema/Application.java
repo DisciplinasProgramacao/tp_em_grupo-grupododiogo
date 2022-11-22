7ade4876b25a91e2481da2a279f6f22b83073afe
xLyMzR <leomardomamedeee@protonmail.com>
2022-11-22 16:32:47 -0300

    Menu tranferido parcialmente e modularizado

--- codigo/FlyCore/src/Sistema/Application.java
+++ codigo/FlyCore/src/Sistema/Application.java
@@ -3,6 +3,15 @@ package Sistema;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+import Utilitarios.*;
+import Utilitarios.AceleradorPts.*;
+import Clientes.Cliente;
+import Passagens.Trecho;
+import Passagens.Voo;
+import Utilitarios.CidadesTrecho;
+
 public class Application {
     static Scanner teclado = new Scanner(System.in);
     // #region utilidades
@@ -25,18 +34,105 @@ public class Application {
     }
     //#endregion
 
+    //#region Trecho
+    private static String escolherCidadesTrecho(String escolha) {
+        limparTela();
+        System.out.println();
+        System.out.println();
 
-    public static int menuPrincipal() {
+        System.out.println("FLY CORE");
+        System.out.println("==========================");
+
+        int posicaoCidade = 0;
+        for (String cidade : escolha == "origem" ? CidadesTrecho.getCidadesOrigem() : CidadesTrecho.getCidadesDestino()) {
+            System.out.println((posicaoCidade + 1) + " - " + cidade);
+            posicaoCidade ++;
+        }
+
+        System.out.println("0 - Cancelar");
+        System.out.print("Escolha a cidade de " + escolha + ": ");
+        try {
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+            return CidadesTrecho.getCidadesOrigem().get(opcao - 1);
+        } catch (InputMismatchException e) {
+            return "";
+        }
+    }
+    private static Trecho cadastradoTrecho() {
+        String cidadeOrigem = "";
+        do {
+            cidadeOrigem = escolherCidadesTrecho("Origem");
+        } while (cidadeOrigem == "");
+
+        String cidadeDestino = "";
+        do {
+            cidadeDestino = escolherCidadesTrecho("Destino");
+        } while (cidadeDestino == "");
+
+        return new Trecho(cidadeOrigem, cidadeDestino);
+    }
+    //endregion
+
+    //#region Voo
+    private static Data cadastrarData(){
+        //Implementar método 
+        return new Data();
+    }
+    private static Voo cadastrarVoo(){
+        Trecho novoTrecho = cadastradoTrecho();
+
+        Data dataVoo = cadastrarData();
+        
+        return new Voo(novoTrecho, dataVoo, 500d);
+    }
+    //endregion
+
+    //# region Multiplicador
+    private static int escolherMultiplicador (){
+        System.out.println();
+        System.out.println("FLY CORE");
+        System.out.println("==========================");
+        System.out.println("1 - Multi Prata");
+        System.out.println("2 - Multi Preto");
+        System.out.println("0 - Cancelar");
+        System.out.print("Digite sua opção: ");
+        try {
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+            return opcao;
+        } catch (InputMismatchException e) {
+            return -1;
+        }
+    }
+    private static IMultiplicavel gerarMultiplicador(Cliente cl){
+        //implementar 
+        int optMulti = escolherMultiplicador();
+        switch(optMulti){
+            case 1:
+                return new MultiplicadorPrata();
+            case 2:
+                return new MultiplicadorPreto();
+            default:
+            return null;
+        }
+    }
+    //#endregion
+
+    //#region Clientes
+    private static void cadastrarCliente(){
+        //implementar
+    }
+    //#endregion
+
+    private static int menuPrincipal() {
         System.out.println();
         System.out.println();
         System.out.println("FLY CORE");
         System.out.println("==========================");
-        System.out.println("1 - Comprar bilhete");
-        System.out.println("2 - Editar bilhete");
-        System.out.println("3 - Verificar pontuação");
-        System.out.println("4 - Exibir bilhete");
-        System.out.println("5 - Exibir todos os bilhetes");
-        
+        System.out.println("1 - Passagens");
+        System.out.println("2 - Cliente");
+        System.out.println("3 - Administrativo");
         System.out.println("0 - Sair");
         System.out.print("Digite sua opção: ");
         try {
@@ -47,9 +143,61 @@ public class Application {
             return -1;
         }
     }
+
+    private static int menuPassagens(){
+        System.out.println();
+        System.out.println("FLY CORE");
+        System.out.println("==========================");
+        System.out.println("1 - Cadastrar Trechos");
+        System.out.println("2 -  Cadastrar Voos");
+        System.out.println("0 - Cancelar");
+        System.out.print("Digite sua opção: ");
+        try {
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+            return opcao;
+        } catch (InputMismatchException e) {
+            return -1;
+        }
+    }
+
+    private static int menuClientes(){
+        System.out.println();
+        System.out.println("FLY CORE");
+        System.out.println("==========================");
+        System.out.println("1 - Cadastrar Cliente");
+        System.out.println("2 - Multiplicador PTS");
+        System.out.println("0 - Cancelar");
+        System.out.print("Digite sua opção: ");
+        try {
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+            return opcao;
+        } catch (InputMismatchException e) {
+            return -1;
+        }     
+    }
+
+    private static int menuADM(){
+        System.out.println();
+        System.out.println("FLY CORE");
+        System.out.println("==========================");
+        System.out.println("1 - Gerar Relatorio (Cliente)");//incluir consulta de bilhete grátis gerado
+        System.out.println("2 - Cliente Maior Pts (Ultimo Ano)");
+        System.out.println("3 - Voos");
+        System.out.println("Vendas");
+        System.out.println("0 - Cancelar");
+        System.out.print("Digite sua opção: ");
+        try {
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+            return opcao;
+        } catch (InputMismatchException e) {
+            return -1;
+        }
+    }
+    
     public static void main(String[] args) {
-        
-        
     }
     
 }
