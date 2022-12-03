9b48efaa0618bce77921fac5793cb4fd26b80339
D Marques <diogomarquesbhdm19@gmail.com>
2022-12-03 15:31:20 -0300

    atualizacao interface IMultiplicavel

--- codigo/FlyCore/src/Utilitarios/AceleradorPts/IMultiplicavel.java
+++ codigo/FlyCore/src/Utilitarios/AceleradorPts/IMultiplicavel.java
@@ -6,23 +6,32 @@ public interface IMultiplicavel {
      * @return pts após o boost do multiplicador.
      */
     public int multiplicar(int pts);
+
     /**
-     * Ativa e Desativa o multiplicador // 
-     *   
+     * Ativa e Desativa o multiplicador
      * @return boolean status do multiplicador após a execução do método
      */
     public boolean on_off();
+
     /**
+     * Retorna o preço relativo a classe de multiplicador que está utilizando o método.
+     */
+    public double getPreco();
+
+     /**
      * Retorna o tipo relativo a classe de multiplicador que está utilizando o método.
      * @return String --> ex: Prata, Preto.
      */
-
-     public double getPreco();
     public String getTipo();
+
     /**
      * Retorna o status atual da instancia de multiplicador que implementa o método.
      * @return boolean --> true : ativo ; false : desativado
      */
     public boolean isAtivo();
+
+    /**
+     * Seta o preço da instancia de multiplicador que implementa o método.
+     */
     public double setPreco(double preco);
 }
